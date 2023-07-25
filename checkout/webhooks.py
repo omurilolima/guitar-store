01118590467a76acf20f50afc869c8d490f54681
Murilo Lima <msl@cesar.school>
2023-07-25 11:53:46 +0000

    Add checkout form data caching in payment intent

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -17,7 +17,7 @@ def webhook(request):
     stripe.api_key = settings.STRIPE_SECRET_KEY
     # Get the webhook data and verify its signature
     payload = request.body
-    sig_header = request.META('HTTP_STRIPE_SIGNATURE')
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
     try:
