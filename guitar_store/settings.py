7044825854d3066142b6e2b69738f7de57ea962e
Murilo Lima <msl@cesar.school>
2023-07-28 17:40:17 +0000

    Move 404.html to errors folder

--- guitar_store/settings.py
+++ guitar_store/settings.py
@@ -23,11 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+# TODO: Change the secret key to an empty string
 SECRET_KEY = os.environ.get(
     'SECRET_KEY', 'sia@o6$oxk466^e8#su23sonr!)e9ijknx%!%u#s0&v!+@mbli')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
