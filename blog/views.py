05cfabea94b9859df2172fd78ffd080a685ba880
Murilo Lima <msl@cesar.school>
2023-07-29 18:19:07 +0000

    Add post_detail

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views import generic, View
 from django.views.generic.detail import DetailView
 from django.http import HttpResponseRedirect
 from .models import Post
+from .forms import CommentForm
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -16,3 +17,77 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/index.html'
     paginate_by = 6
+
+
+class PostDetail(View):
+    """
+    Displays a individual blog post on a single page,
+    including feature to like and add comment.
+    """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if request.user.is_authenticated and post.likes.filter(id=self.request.user.id).exists():  # noqa
+            liked = True
+
+        return render(
+            request,
+            'blog/post_detail.html',
+            {
+                'post': post,
+                'comments': comments,
+                'commented': False,
+                'liked': liked,
+                'comment_form': CommentForm()
+            }
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            'blog/post_detail.html',
+            {
+                'post': post,
+                'comments': comments,
+                'commented': True,
+                'liked': liked,
+                'comment_form': CommentForm()
+            }
+        )
+
+
+class PostLike(LoginRequiredMixin, View):
+    """
+    View to remove or add like on post detail page
+    """
+
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
