b0414d7ba9bc929dcdc5f8741152d9cbf5d01ae2
Murilo Lima <msl@cesar.school>
2023-08-01 18:29:18 +0000

    Update settings

--- guitar_store/settings.py
+++ guitar_store/settings.py
@@ -200,7 +200,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'http://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
 
     # Static and Media Files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
@@ -211,7 +211,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'http://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'http://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
