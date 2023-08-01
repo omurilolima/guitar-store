994501a0a06d1b9130c940e636c18cb531dbcefd
Murilo Lima <msl@cesar.school>
2023-08-01 21:44:42 +0000

    Removing data_base_url from settings

--- guitar_store/settings.py
+++ guitar_store/settings.py
@@ -134,21 +134,17 @@ WSGI_APPLICATION = "guitar_store.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse('postgres://duxuatll:plAy8Q3nMYLQPfAQxDFfhbGkNfFEMvx_@trumpet.db.elephantsql.com/duxuatll')
-}
-
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": BASE_DIR / "db.sqlite3",
-#         }
-#     }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
