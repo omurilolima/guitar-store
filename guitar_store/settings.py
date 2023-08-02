f94e1756258c5e1c086f0da534e1da8c13339211
Murilo Lima <msl@cesar.school>
2023-08-02 11:50:47 +0000

    Update settings for the new AWS bucket

--- guitar_store/settings.py
+++ guitar_store/settings.py
@@ -192,7 +192,7 @@ if 'USE_AWS' in os.environ:
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'murilo-guitar-shop'
-    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
